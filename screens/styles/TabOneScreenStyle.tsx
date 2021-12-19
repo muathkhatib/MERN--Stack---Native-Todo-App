cad18d8c1245fb8f0019ebc78c3f8f1ab6948934
muathkhatib <mkhatib.dev@gmail.com>
2021-12-19 13:41:21 +0200

    render todos list - tab one sc

--- screens/styles/TabOneScreenStyle.tsx
+++ screens/styles/TabOneScreenStyle.tsx
@@ -1,22 +1,23 @@
 /* eslint-disable import/namespace */
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: 'bold',
-    },
-    separator: {
-      marginVertical: 30,
-      height: 1,
-      width: '80%',
-    },
-  });
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  flatList:{
+    width: "100%",
+  }
+});
 
-  export default styles
+export default styles;
